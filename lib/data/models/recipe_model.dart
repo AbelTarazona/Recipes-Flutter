46d5a170e1a0718735af784ea853323ba615ea6c
Abel Tarazona <abel.tarazona@urp.edu.pe>
2024-06-05 23:07:19 -0500

    feat(data): add models

---
+++ lib/data/models/recipe_model.dart
@@ -0,0 +1,53 @@
+class RecipeModel {
+  int id;
+  String name;
+  List<String> ingredients;
+  List<String> instructions;
+  int prepTimeMinutes;
+  int cookTimeMinutes;
+  int servings;
+  String difficulty;
+  String cuisine;
+  int caloriesPerServing;
+  List<String> tags;
+  String image;
+  double rating;
+  int reviewCount;
+  List<String> mealType;
+
+  RecipeModel({
+    required this.id,
+    required this.name,
+    required this.ingredients,
+    required this.instructions,
+    required this.prepTimeMinutes,
+    required this.cookTimeMinutes,
+    required this.servings,
+    required this.difficulty,
+    required this.cuisine,
+    required this.caloriesPerServing,
+    required this.tags,
+    required this.image,
+    required this.rating,
+    required this.reviewCount,
+    required this.mealType,
+  });
+
+  factory RecipeModel.fromJson(Map<String, dynamic> json) => RecipeModel(
+        id: json["id"],
+        name: json["name"],
+        ingredients: List<String>.from(json["ingredients"].map((x) => x)),
+        instructions: List<String>.from(json["instructions"].map((x) => x)),
+        prepTimeMinutes: json["prepTimeMinutes"],
+        cookTimeMinutes: json["cookTimeMinutes"],
+        servings: json["servings"],
+        difficulty: json["difficulty"],
+        cuisine: json["cuisine"],
+        caloriesPerServing: json["caloriesPerServing"],
+        tags: List<String>.from(json["tags"].map((x) => x)),
+        image: json["image"],
+        rating: json["rating"].toDouble(),
+        reviewCount: json["reviewCount"],
+        mealType: List<String>.from(json["mealType"].map((x) => x)),
+      );
+}
