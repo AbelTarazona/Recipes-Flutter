2e4238aa4dcce61347e5b1088b44eeb807b85846
Abel Tarazona <abel.tarazona@urp.edu.pe>
2024-06-06 21:28:33 -0500

    feat(presentation): update assets

--- lib/core/config/app_colors.dart
+++ lib/core/config/app_colors.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color bianca = Color(0xFFfaf6eb);
   static const Color codGray = Color(0xFF1e1e1e);
+  static const Color dawn = Color(0xFFacaaa3);
+  static const Color mossGreen = Color(0xFFb5dca8);
+  static const Color albescentWhite = Color(0xFFf3d7c7);
 }
