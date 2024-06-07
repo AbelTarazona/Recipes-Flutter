45a3b1ab98e3e149f514361cbc8307d7fc7d2d62
Abel Tarazona <abel.tarazona@urp.edu.pe>
2024-06-06 22:36:06 -0500

    feat(home): add tap action to search

--- lib/presentation/home/widgets/searcher.dart
+++ lib/presentation/home/widgets/searcher.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:recipes_aplazo/core/config/app_colors.dart';
+import 'package:recipes_aplazo/presentation/search/search_page.dart';
 
 class Searcher extends StatelessWidget {
   const Searcher({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(100),
-        border: Border.all(color: AppColors.codGray),
-      ),
-      child: Row(
-        children: [
-          const Icon(Icons.search_rounded),
-          const SizedBox(width: 10),
-          Text(
-            'Search for recipes...',
-            style: TextStyle(fontSize: 18),
-          )
-        ],
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(SearchPage.route());
+      },
+      borderRadius: BorderRadius.circular(100),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          border: Border.all(color: AppColors.codGray),
+        ),
+        child: const Row(
+          children: [
+            Icon(Icons.search_rounded),
+            SizedBox(width: 10),
+            Text(
+              'Search for recipes...',
+              style: TextStyle(fontSize: 18),
+            )
+          ],
+        ),
       ),
     );
   }
